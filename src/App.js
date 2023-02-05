f771d833fe990fed6203aaf5dfa8f54027b009bf
houssemboulaares <houssem.boulaares.123@gmail.com>
2023-02-05 09:12:54 +0100

    workshop

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Menu from './Menu';
+import Contact from './Contact';
+import BasicExample from './BasicExample';
+
+function App() {
+  return (
+    <>
+    <BasicExample></BasicExample>
+    <Navbar></Navbar>
+    <Menu></Menu>
+    <Contact></Contact>
+
+    </>
+  );
+}
+
+export default App;
